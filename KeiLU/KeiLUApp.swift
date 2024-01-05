46b162cee48e46947a4eec40dbac3b73e4dd5c49
zhany <zhanybeksalgarin@gmail.com>
2024-01-05 11:20:04 -0800

    first commit

---
+++ KeiLU/KeiLUApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeiLUApp.swift
+//  KeiLU
+//
+//  Created by zhanybek salgarin on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct KeiLUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
