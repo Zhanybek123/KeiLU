a5dc7c393e4f0d57077bb491cc7923420ba1a7e5
zhany <zhanybeksalgarin@gmail.com>
2024-01-08 13:28:46 -0800

    Add pircture, days of the week

--- KeiLU/ContentView.swift
+++ KeiLU/ContentView.swift
@@ -8,11 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    
     var body: some View {
-        HStack{
-            Image(systemName: "loading")
-            Text("January")
+        VStack{
+            Image("myFace")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 150, height: 150)
+                .clipShape(.capsule)
             
+            Text("Marco Luciano")
+                .font(.title)
+                .bold()
+            
+            Rectangle()
+                .frame(height: 1)
+            
+            Text("Select day")
+                .font(.title2)
+                .bold()
+            
+            HStack{
+                Button {
+                    print("something")
+                } label: {
+                    Image(systemName: "lessthan")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 16, height: 28)
+                }
+                
+                Text("January 2024")
+                    .font(.title2)
+                    .padding([.leading, .trailing], 20)
+                
+                Button {
+                    print("something")
+                } label: {
+                    Image(systemName: "greaterthan")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 16, height: 28)
+                }
+            }
+            HStack {
+                ForEach(days, id: \.self) { day in
+                    Text(day)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            .padding(5)
         }
     }
 }
