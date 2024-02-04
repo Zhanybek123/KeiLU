9c989ed93842cb7542a8583d8fc41dfdf86bce67
zhany <zhanybeksalgarin@gmail.com>
2024-02-04 11:55:48 -0800

    Add navigations and buttons

--- KeiLU/ArtistsListView.swift
+++ KeiLU/ArtistsListView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ArtistModel: Hashable {
-    let logo: String
+    var logo: String
     let name: String
     let minimumPrice: Int
     let averageDuration: Int
@@ -20,7 +20,7 @@ struct ArtistsListView: View {
     @State var artists: [ArtistModel] = [
         ArtistModel(logo: "nailArtist2",
                     name: "LucysSpot",
-                    minimumPrice: 45, 
+                    minimumPrice: 45,
                     averageDuration: 40,
                     sesrvices: ["Face" , "Nails", "Massage"]),
         ArtistModel(logo: "nailArtist3",
@@ -40,102 +40,126 @@ struct ArtistsListView: View {
                     sesrvices: ["Face" , "Nails", "Massage"])
     ]
     
+    @State var selectedArtist = ArtistModel(logo: "nailArtist",
+                                            name: "LucysSpot",
+                                            minimumPrice: 45,
+                                            averageDuration: 40,
+                                            sesrvices: ["Face" , "Nails", "Massage"])
+    
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                ZStack(alignment: .bottomLeading) {
-                    Image("nailArtist")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(maxWidth: geometry.size.width)
-                        .ignoresSafeArea()
-
-                    VStack(alignment: .leading, spacing: 3) {
-                        Text("HAIR/NAIL artist")
-                            .fontWeight(.regular)
-                            .foregroundStyle(.white)
-                        Text("FanFan Tullip")
-                            .font(.title3)
-                            .bold()
-                            .foregroundStyle(.white)
-                        Text("At home, will travel")
-                            .fontWeight(.regular)
-                            .foregroundStyle(.white)
+        NavigationStack {
+            GeometryReader { geometry in
+                VStack {
+                    ZStack(alignment: .bottomLeading) {
+                        Image(selectedArtist.logo)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(maxWidth: geometry.size.width, maxHeight: 300)
+                            .ignoresSafeArea()
+                        
+                        VStack(alignment: .leading, spacing: 3) {
+                            Text("HAIR/NAIL artist")
+                                .fontWeight(.regular)
+                                .foregroundStyle(.white)
+                            Text("FanFan Tullip")
+                                .font(.title3)
+                                .bold()
+                                .foregroundStyle(.white)
+                            Text("At home, will travel")
+                                .fontWeight(.regular)
+                                .foregroundStyle(.white)
+                        }
+                        .padding(EdgeInsets(top: 8, leading: 24, bottom: 8, trailing: 24))
+                        .background(Color.black.opacity(0.6))
+                        .clipShape(RoundedRectangle(cornerRadius: 16))
+                        .padding(EdgeInsets(top: 6, leading: 16, bottom: 6, trailing: 16))
+                        
                     }
-                    .padding(EdgeInsets(top: 8, leading: 24, bottom: 8, trailing: 24))
-                    .background(Color.black.opacity(0.6))
-                    .clipShape(RoundedRectangle(cornerRadius: 16))
-                    .padding(EdgeInsets(top: 6, leading: 16, bottom: 6, trailing: 16))
+                    .frame(maxWidth: geometry.size.width, maxHeight: 300)
                     
-                }
-                .frame(maxWidth: geometry.size.width, maxHeight: 300)
-                
-                HStack(spacing: 16) {
-                    VStack {
-                        Image(systemName: "text.bubble")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 30, height: 30, alignment: .center)
-                        Text("Text")
-                            .fontWeight(.regular)
-                            .font(Font(CTFont.init(.label, size: 13)))
-                    }
-                    VStack {
-                        Image(systemName: "mappin.and.ellipse")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 30, height: 30, alignment: .center)
-                        Text("Direction")
-                            .fontWeight(.regular)
-                            .font(.system(size: 13, weight: .regular))
-                            
-                    }
-                    Spacer()
-                    VStack {
-                        HStack {
-                            Image(systemName: "star")
-                                .foregroundStyle(Color.blue)
-                            Text("\(4)")
-                                .foregroundStyle(Color.blue)
-                        }
-                        .padding(EdgeInsets(top: 6, leading: 10, bottom: 6, trailing: 10))
-                        .overlay {
-                            RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.blue, lineWidth: 1)
-                        }
-                        Text("100+ rating")
-                            .font(.system(size: 13, weight: .regular))
-                            .foregroundStyle(Color.blue)
-                    }
-                }
-                .padding(20)
-                List {
-                    ForEach(artists, id: \.self) { artist in
-                        HStack {
-                            Image(artist.logo)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(maxWidth: 100, maxHeight: 100)
-                                .clipShape(RoundedRectangle(cornerRadius: 14))
-                            VStack(alignment: .leading) {
-                                Text(artist.name)
-                                    .font(.title3)
-                                    .bold()
-                                Text("$\(artist.minimumPrice)")
-                                    .foregroundStyle(.gray)
-                                Spacer()
-                                Text("\(artist.averageDuration) Mins")
-                                    .foregroundStyle(.gray)
-                                    .font(.system(size: 13))
+                    HStack(spacing: 16) {
+                        NavigationLink {
+                            EmptyView()
+                        } label: {
+                            VStack {
+                                Image(systemName: "text.bubble")
+                                    .resizable()
+                                    .scaledToFit().foregroundStyle(.black)
+                                    .foregroundStyle(.black)
+                                    .frame(width: 30, height: 30, alignment: .center)
+                                Text("Text")
+                                    .fontWeight(.regular)
+                                    .font(Font(CTFont.init(.label, size: 13)))
+                                    .foregroundStyle(.black)
                             }
-                            .padding(EdgeInsets(top: 10, leading: 2, bottom: 10, trailing: 2))
                         }
-                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)) // Remove list row insets
-//                        .padding()
-                        .frame(height: 100)
+                        NavigationLink {
+                            EmptyView()
+                        } label: {
+                            VStack {
+                                Image(systemName: "mappin.and.ellipse")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .foregroundStyle(.black)
+                                    .frame(width: 30, height: 30, alignment: .center)
+                                Text("Direction")
+                                    .fontWeight(.regular)
+                                    .font(.system(size: 13, weight: .regular))
+                                    .foregroundStyle(.black)
+                                
+                            }
+                        }
+                        Spacer()
+                        VStack {
+                            HStack {
+                                Image(systemName: "star")
+                                    .foregroundStyle(Color.blue)
+                                Text("\(4)")
+                                    .foregroundStyle(Color.blue)
+                            }
+                            .padding(EdgeInsets(top: 6, leading: 10, bottom: 6, trailing: 10))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.blue, lineWidth: 1)
+                            }
+                            Text("100+ rating")
+                                .font(.system(size: 13, weight: .regular))
+                                .foregroundStyle(Color.blue)
+                        }
+                    }
+                    .padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 20))
+                    List {
+                        ForEach(artists, id: \.self) { artist in
+                            Button(action: {
+                                selectedArtist.logo = artist.logo
+                                print("Tapped on \(artist.name)")
+                            }) {
+                                HStack {
+                                    Image(artist.logo)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(maxWidth: 100, maxHeight: 100)
+                                        .clipShape(RoundedRectangle(cornerRadius: 14))
+                                    VStack(alignment: .leading) {
+                                        Text(artist.name)
+                                            .font(.title3)
+                                            .foregroundStyle(.black)
+                                            .bold()
+                                        Text("$\(artist.minimumPrice)")
+                                            .foregroundStyle(.gray)
+                                        Spacer()
+                                        Text("\(artist.averageDuration) Mins")
+                                            .foregroundStyle(.gray)
+                                            .font(.system(size: 13))
+                                    }
+                                    .padding(EdgeInsets(top: 10, leading: 2, bottom: 10, trailing: 2))
+                                }
+                            }
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                            .frame(height: 100)
+                        }
                     }
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
         }
     }
